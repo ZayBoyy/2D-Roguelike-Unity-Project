3cb4d9e1b22f9bc83ceb13ab4b69fc8af6ee88d1
e-n-w <elijahw721@gmail.com>
2022-11-26 18:02:48 -0700

    Simple ranged ai and projectile

---
+++ RPG/Assets/Scripts/ProjectileAI.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileAI : MonoBehaviour
+{
+    public Transform transform;
+
+    public float speed = 1.5f;
+
+    public float timer = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.up * Time.deltaTime * speed;
+        timer -= Time.deltaTime;
+        if(timer <= 0){
+            Destroy(transform.root.gameObject);
+        }
+    }
+}
---
+++ RPG/Assets/Scripts/RangedAI.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedAI : MonoBehaviour
+{
+
+    public float speed = 1.0f;
+
+    public Transform enemyT;
+    public Vector3 pos;
+
+    public GameObject player;
+    public Transform playerT;
+    public Vector3 playerPos;
+
+    public float distToPlayer;
+
+    public GameObject projectile;
+
+    public const float maxProjTimer = 2f;
+    public float projTimer = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyT = GetComponent<Transform>();
+        pos = enemyT.position;
+
+        player = GameObject.Find("Player?");
+        playerT = player.transform;
+        playerPos = playerT.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPos = playerT.position;
+        pos = enemyT.position;
+        Vector3 displacementToPlayer = playerPos - pos;
+        distToPlayer = displacementToPlayer.magnitude;
+        Vector3 directionToPlayer = displacementToPlayer / distToPlayer;
+
+        if(distToPlayer <= 5f){
+            if(distToPlayer <= 2f){
+                enemyT.position += -1*speed*directionToPlayer*Time.deltaTime;
+            }else{
+                if(projTimer <= 0){
+                    GameObject proj;
+                    proj = Instantiate(projectile, enemyT.position + enemyT.forward, enemyT.rotation);
+                    proj.transform.up = directionToPlayer;
+                    projTimer = maxProjTimer;
+                }else{
+                    projTimer -= Time.deltaTime;
+                } 
+            } 
+        }
+    }
+}
